add9038292fe4109e210be9f332423eceb6f54b3
TsingMetal <420895449@qq.com>
2017-10-09 01:12:16 +0000

    urls refactored

---
+++ lists/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import url
+from lists import views
+
+urlpatterns = [
+    url(r'^new$', views.new_list, name='new_list'),
+    url(r'^([1-9]+)/$', views.view_list, name='view_list'),
+    url(r'^([1-9]+)/add_item$', views.add_item, name='add_item'),
+]
