5381aa6a56a956c143016894540f6bdbf2201243
tsing <tsing@tsing>
2017-10-18 23:30:15 +0800

    hack FT runner to be able to test staging

--- superlists/settings.py
+++ superlists/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'tv0bk7wqu(i6r=i0swro@iboamd-11yxyftu4k+7*2!l=hunv+'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
