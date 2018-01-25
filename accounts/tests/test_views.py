706b4d47a9ce35f85bdb4fb173ef6247bd670042
mylaptop <tsing_w@163.com>
2018-01-26 00:36:22 +0800

    Create Token associated with a given email address

--- accounts/tests/test_views.py
+++ accounts/tests/test_views.py
@@ -3,6 +3,9 @@ from unittest.mock import call, patch
 from django.test import TestCase
 
 import accounts.views
+from accounts.models import Token
+
+
 
 
 class SendLoginEmailViewTest(TestCase):
@@ -23,10 +26,10 @@ class SendLoginEmailViewTest(TestCase):
 
         self.assertEqual(mock_send_mail.called, True)
         (subject, body, from_email, to_list), kwargs = \
-                mock_send_mail.call_args
+            mock_send_mail.call_args
         self.assertEqual(subject, 'Your login link for Superlists')
         self.assertEqual(from_email, 'noreply@superlists')
-        self.assertEqual(to_list, ['edith@example.com',])
+        self.assertEqual(to_list, ['edith@example.com', ])
 
     def test_adds_success_message(self):
         response = self.client.post(
@@ -42,15 +45,31 @@ class SendLoginEmailViewTest(TestCase):
         )
         self.assertEqual(message.tags, 'success')
 
-    @patch('accounts.views.messages')
-    def test_adds_success_message_with_mocks(self, mock_messages):
-        response = self.client.post(
+
+class LoginViewTest(TestCase):
+
+    def test_redirects_to_home_page(self):
+        response = self.client.get('/accounts/login?token=abcd123')
+        self.assertRedirects(response, '/')
+
+    def test_creates_token_associated_with_email(self):
+        self.client.post(
+                '/accounts/send_login_email',
+                data={'email': 'edith@example.com'}
+        )
+        token = Token.objects.first()
+        self.assertEqual(token.email, 'edith@example.com')
+
+    @patch('accounts.views.send_mail')
+    def test_sends_link_to_login_using_token_uid(self, mock_send_mail):
+        self.client.post(
                 '/accounts/send_login_email',
                 data={'email': 'edith@example.com'}
         )
 
-        expected = "Check your email, we've sent you a link you can use to log in."
-        self.assertEqual(
-            mock_messages.success.call_args,
-            call(response.wsgi_request, expected),
-        )
+        token = Token.objects.first()
+        expected_url = \
+            f'http://testserver/accounts/login?token={token.uid}'
+        (subject, body, from_email, to_list), kwargs = \
+            mock_send_mail.call_args
+        self.assertIn(expected_url, body)
